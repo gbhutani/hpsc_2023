ea4db7539a49c836ad10781b0fbbfaebd723d024
Gaurav <bhutani_gaurav@yahoo.com>
2023-02-08 22:11:10 +0530

    Added a convergence test with a large kmax.

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,7 +1,12 @@
-x=2.0
+x=0.0001
 s=1.
-kmax=3
+kmax=100
+tol=1.0e-14
 for k in range(kmax):
 	print("At iteration number %s, s= %s" %(k,s))
+	s0=s
 	s = 0.5*(s+x/s)
+	delta_s=s-s0
+	if(abs(delta_s)/x)<tol:
+		break
 print("After %s iterations, s=%s" %(k+1,s))
