08122530bfdf968c4089e7c2b9d352cdb7118613
Gaurav <bhutani_gaurav@yahoo.com>
2023-02-08 22:03:32 +0530

    Modified script to print iterates

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,5 +1,7 @@
-x=16.0
-s=2.
-for k in range(6):
-    s = 0.5*(s+x/s)
-print(s)
+x=2.0
+s=1.
+kmax=3
+for k in range(kmax):
+	print("At iteration number %s, s= %s" %(k,s))
+	s = 0.5*(s+x/s)
+print("After %s iterations, s=%s" %(k+1,s))
