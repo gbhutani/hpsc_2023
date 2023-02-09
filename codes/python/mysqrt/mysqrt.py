200710f6a7bd0ca72ed15921fdd5f8a21ebedaaa
Gaurav <bhutani_gaurav@yahoo.com>
2023-02-09 14:56:31 +0530

    Added docstrings

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,4 +1,13 @@
+"""
+A python module for calculating square root using the 
+Newton's method implemented in ME522 class
+"""
+
 def sqrtNT(x):
+	"""
+	The actual sqrtNT function
+	Takes one input x: the number whose square root is to be calculated
+	"""
 	s=1.
 	kmax=100
 	tol=1.0e-14
