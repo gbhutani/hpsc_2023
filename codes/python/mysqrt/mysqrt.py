6c2552edcb613af8c6dbb006f19266b689d18424
Gaurav <bhutani_gaurav@yahoo.com>
2023-02-09 14:39:00 +0530

    Added fixed format in output

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,12 +1,12 @@
-x=0.0001
-s=1.
-kmax=100
-tol=1.0e-14
-for k in range(kmax):
-	print("At iteration number %s, s= %s" %(k,s))
-	s0=s
-	s = 0.5*(s+x/s)
-	delta_s=s-s0
-	if(abs(delta_s)/x)<tol:
-		break
-print("After %s iterations, s=%s" %(k+1,s))
+def sqrtNT(x):
+	s=1.
+	kmax=100
+	tol=1.0e-14
+	for k in range(kmax):
+		print("At iteration number %s, s= %20.15f" %(k,s))
+		s0=s
+		s = 0.5*(s+x/s)
+		delta_s=s-s0
+		if(abs(delta_s)/x)<tol:
+			break
+	print("After %s iterations,  s=%20.15f" %(k+1,s))
