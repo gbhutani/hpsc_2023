7d41fac0733a645849033564e666d8f934eebadc
Gaurav Bhutani <bhutani@Gauravs-MacBook-Air.local>
2023-02-11 00:19:28 +0530

    Corrected an issue of x inside parenthesis. Added an option to disable zero and negative values check to analyse the assert statement.

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -3,19 +3,24 @@ A python module for calculating square root using the
 Newton's method implemented in ME522 class
 """
 
-def sqrtNT(x,debug=False):
+def sqrtNT(x,debug=False,specialCases=True):
 	"""
 	The actual sqrtNT function
-	Takes one input x: the number whose square root is to be calculated
+	Inputs
+	x: the number whose square root is to be calculated
+	debug: True if iteration details need to be printed. Default value is False
+	specialCases: False to disable zero and negative number check. Default value is True
+
 	"""
 	from numpy import nan
 	
-	if x==0.:
-		return 0.
-	elif x<0.:
-		print("An error has occured since you have passed a negative value for x which does \
-		not have a real square root")
-		return nan
+	if specialCases:
+		if x==0.:
+			return 0.
+		elif x<0.:
+			print("An error has occured since you have passed a negative value for"
+				"x which does not have a real square root")
+			return nan
 	assert x>0., "Input not recognised"	
 	s=1.
 	kmax=100
@@ -26,7 +31,7 @@ def sqrtNT(x,debug=False):
 		s0=s
 		s = 0.5*(s+x/s)
 		delta_s=s-s0
-		if(abs(delta_s)/x)<tol:
+		if(abs(delta_s/x))<tol:
 			break
 	if debug:
 		print("After %s iterations,  s=%20.15f" %(k+1,s))
