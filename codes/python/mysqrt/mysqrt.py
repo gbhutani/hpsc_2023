d0cd3a2c9b5893f8544fe074bc0fd69fe3737c06
Gaurav <bhutani_gaurav@yahoo.com>
2023-02-10 21:40:12 +0530

    Added debug flag and return value to the function

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -3,7 +3,7 @@ A python module for calculating square root using the
 Newton's method implemented in ME522 class
 """
 
-def sqrtNT(x):
+def sqrtNT(x,debug=False):
 	"""
 	The actual sqrtNT function
 	Takes one input x: the number whose square root is to be calculated
@@ -12,10 +12,13 @@ def sqrtNT(x):
 	kmax=100
 	tol=1.0e-14
 	for k in range(kmax):
-		print("At iteration number %s, s= %20.15f" %(k,s))
+		if debug:
+			print("At iteration number %s, s= %20.15f" %(k,s))
 		s0=s
 		s = 0.5*(s+x/s)
 		delta_s=s-s0
 		if(abs(delta_s)/x)<tol:
 			break
-	print("After %s iterations,  s=%20.15f" %(k+1,s))
+	if debug:
+		print("After %s iterations,  s=%20.15f" %(k+1,s))
+	return s
