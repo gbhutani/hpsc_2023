8cc78e3db1a14abb22c3a5715daf427309296ab5
Gaurav Bhutani <bhutani@Gauravs-MacBook-Air.local>
2023-02-27 10:34:05 +0530

    next version of debugging with lots of print statements

--- codes/python/debug_pdb/debugdemo.py
+++ codes/python/debug_pdb/debugdemo.py
@@ -1,11 +1,16 @@
-x=3
+x="three"
 y=-22.0
 
 def f(z):
 	x=z+10
+	print(f"**** inside the function f, after the execution of assign statement: x={x}, y={y},z={z}")
 	return x
 
+print(f"**** before calling function f: x={x}, y={y}")
+
 y=f(x)
+
+print(f"**** after calling the function f: x={x}, y={y}")
 
 print(f"x={x}")
 print(f"y={y}")
