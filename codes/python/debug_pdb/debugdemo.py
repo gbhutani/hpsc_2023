dfaf897477f0e3e40d13b189d18eb212953ff7e2
Gaurav Bhutani <bhutani@Gauravs-MacBook-Air.local>
2023-02-27 11:09:14 +0530

    use of pdb set_trace() to debug

--- codes/python/debug_pdb/debugdemo.py
+++ codes/python/debug_pdb/debugdemo.py
@@ -1,16 +1,12 @@
-x="three"
+x=3.0
 y=-22.0
 
 def f(z):
+	import pdb; pdb.set_trace()
 	x=z+10
-	print(f"**** inside the function f, after the execution of assign statement: x={x}, y={y},z={z}")
 	return x
 
-print(f"**** before calling function f: x={x}, y={y}")
-
 y=f(x)
-
-print(f"**** after calling the function f: x={x}, y={y}")
 
 print(f"x={x}")
 print(f"y={y}")
