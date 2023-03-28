84868c7ab62d699d111221c5900a6754eb40f4e8
s22033 <s22033@students.iitmandi.ac.in>
2023-03-28 22:45:15 +0530

    added exercise

--- exercise/lecture7intro/mysqrt.py
+++ exercise/lecture7intro/mysqrt.py
@@ -46,7 +46,3 @@ def test_main():
 		s_numpy=sqrt(x)
 		print("sqrtNT s = %20.15e, numpy s = %20.15e" %(s,s_numpy))
 		assert abs(s-s_numpy) < 1e-14, "Your sqrt does not agree with numpy sqrt"
-if __name__ == "__main__":
-    print("Running test... ")
-    test_main()
-
